b5b1b68a13b888b7590b1b0aee051fdf160508e4
siamislam07 <siamislam1970@gmail.com>
2023-08-20 11:01:52 +0600

    add items to cart added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-console.log('angta lagaiya disi');
+document.getElementById('coupon-input').addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const applybtn = document.getElementById('btn-coupon');
+    if(text === 'SELL200'){
+        applybtn.removeAttribute('disabled')
+    }else{
+        applybtn.setAttribute('disabled', true);
+    }
+})
+
+function handleClickBtn(target){
+    const addToCartContainer = document.getElementById('addToCart')
+    const itemName = target.childNodes[2].nextSibling.innerText;
+    const li = document.createElement("li")
+    li.innerText = itemName
+    addToCartContainer.appendChild(li)
+} 
